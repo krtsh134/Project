bdb7ea8272469d12b7ef145cb428e715caac117a
krtsh134 <internetdevil2@gmail.com>
2024-12-14 13:08:17 +0300

    lalala

--- check_bd.py
+++ check_bd.py
@@ -7,7 +7,7 @@ cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
 tables = cursor.fetchall()
 print("Tables:", tables)
 
-cursor.execute("SELECT * FROM MealPlans;") #после FROM - имя любой таблицы
+cursor.execute("SELECT * FROM TrainPlans;") #после FROM - имя любой таблицы
 rows = cursor.fetchall()
 
 for row in rows:
