f1c663e6876995583d5822beec136f39cb7faaf5
lizkafe <9065315553liza@gmail.com>
2024-12-19 12:03:57 +0300

    Убрала пометки

--- test_addnewfoods.py
+++ test_addnewfoods.py
@@ -11,7 +11,6 @@ class TestNutritionalFunctions(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """Создание тестовой базы данных перед выполнением всех тестов"""
         cls.connection = sqlite3.connect(TEST_DB)
         cls.cursor = cls.connection.cursor()
         cls.cursor.execute("""
@@ -29,17 +28,14 @@ class TestNutritionalFunctions(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """Удаление тестовой базы данных после выполнения всех тестов"""
         cls.connection.close()
         os.remove(TEST_DB)
 
     def setUp(self):
-        """Очистка таблицы перед каждым тестом"""
         self.cursor.execute("DELETE FROM Products")
         self.connection.commit()
 
     def test_add_newfoods(self):
-        """Тест добавления нового продукта в базу данных"""
         add_newfoods("Яблоко", 47, 0.4, 0.4, 9.8, 100, dbname=TEST_DB)
         self.cursor.execute("SELECT * FROM Products WHERE name = 'Яблоко'")
         result = self.cursor.fetchone()
