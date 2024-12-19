450eddf6b318cbfd05a8a446c69e52c45f94a39f
lizkafe <9065315553liza@gmail.com>
2024-12-19 03:37:01 +0300

    добавила документацию

--- count_nutritional_value.py
+++ count_nutritional_value.py
@@ -1,10 +1,25 @@
 from db import *
-from project import *
 from tkinter import messagebox
 
 
+def add_newfoods(name, kilocalories, protein_gramms, fat_gramms, carbohydrates_gramms, serving_size_gramms, dbname='health_control.db'):
+    """Добавляет новую запись о продукте в базу данных SQLite.
 
-def add_newfoods(name, kilocalories, protein_gramms, fat_gramms, carbohydrates_gramms, serving_size_gramms, dbname='health_control.db', run_counter=True):
+    :param name: Название продукта.
+    :type name: str
+    :param kilocalories: Количество килокалорий на порцию.
+    :type kilocalories: int or float
+    :param protein_gramms: Количество белков в граммах на порцию.
+    :type protein_gramms: int or float
+    :param fat_gramms: Количество жиров в граммах на порцию.
+    :type fat_gramms: int or float
+    :param carbohydrates_gramms: Количество углеводов в граммах на порцию.
+    :type carbohydrates_gramms: int or float
+    :param serving_size_gramms: Размер порции в граммах.
+    :type serving_size_gramms: int or float
+    :param dbname: Имя файла базы данных SQLite. По умолчанию используется 'health_control.db'.
+    :type dbname: str
+    """
     cnct = sqlite3.connect(dbname)
     cursor = cnct.cursor()
     sql = """INSERT INTO Products 
@@ -20,3 +35,8 @@ def add_newfoods(name, kilocalories, protein_gramms, fat_gramms, carbohydrates_g
 
     cnct.close()
     print("Ввод данных завершен")
+
+
+
+
+
