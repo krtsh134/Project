4a7b02f054c8d67fdcab4a1ab86387dba9602758
lizkafe <9065315553liza@gmail.com>
2024-12-19 05:04:58 +0300

    Отформатировала

--- test_back_liza.py
+++ test_back_liza.py
@@ -2,10 +2,6 @@ import unittest
 from unittest.mock import patch, MagicMock
 from project import back_to_add_data_window3, back_to_add_data_window1
 
-
-
-
-
 class TestBackToDataWindow3(unittest.TestCase):
 
     @patch('project.counter_kcal_window', new_callable=MagicMock)
@@ -22,16 +18,10 @@ class TestBackToAddDataWindow1(unittest.TestCase):
     @patch('project.add_newfood_window', new_callable=MagicMock)
     @patch('project.add_data_window', new_callable=MagicMock)
     def test_back_to_add_data_window1(self, mock_add_data_window, mock_add_newfood_window):
-        # Arrange
         mock_add_newfood_window.is_add_newfood_window_open = True
-
-        # Act
         back_to_add_data_window1()
-
-        # Assert
         mock_add_newfood_window.destroy.assert_called_once()
         mock_add_data_window.deiconify.assert_called_once()
-
 
 
 
